1edda3f821302f784614a83c9cbdee86feced634
Vinicius Candido Chaves <viniciuschaves1007@gmail.com>
2026-05-26 21:50:29 -0300

    finish

---
+++ Botões/botoes.js
@@ -0,0 +1,11 @@
+function botaoClicado(botao){
+    alert("O " + botao + " foi clicado!")
+}
+
+function adicionarAoCarrinho(produto){
+    console.log("Produto adicionado: ", produto)
+}
+
+adicionarAoCarrinho("🍌")
+adicionarAoCarrinho("🍎")
+adicionarAoCarrinho("🍐")
---
+++ lista de exercicios/exer25.js
@@ -0,0 +1,9 @@
+const media = function (x,y){
+    let resultado = (x = y)/2
+
+    return resultado
+}
+
+console.log(media(10,5))
+console.log(media(2,9))
+console.log(media(100,476))
