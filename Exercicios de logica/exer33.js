06467a6660847f2787aefbbc28235cd49397f414
Vinicius Candido Chaves <viniciuschaves1007@gmail.com>
2026-06-15 19:54:15 -0300

    Organização

--- Exercicios de logica/exer33.js
+++
@@ -1,5 +0,0 @@
-//Escreva uma função construtora chamada Produto que receba nome (string), preco (number)
-//  e estoque (number). Adicione um método vender(quantidade), que recebe a quantidade 
-// como parâmetro, diminua o estoque caso haja produtos suficientes.
-
-
--- Projetos de javascript/cartas/cartas.js
+++ Projetos de javascript/cartas/cartas.js
@@ -1,11 +1,13 @@
 function criarCarta(){
     let inputNome = document.getElementById("nome-da-carta");
     let inputImagem = document.getElementById("imagem-da-carta");
+   
 
     let carta = document.createElement("div")
 
     carta.innerHTML="<p>"+ inputNome.value + "</p>"
-    carta.innerHTML += '<img src="' + inputImagem.value + '"/>'
+    carta.innerHTML += '<img width"200"src="' + inputImagem.value + '"/>'
+    
 
     let body = document.querySelector("body")
     body.appendChild(carta)
